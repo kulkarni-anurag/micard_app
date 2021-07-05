443765c3c4b8852e4af17516984106244a97bdb6
kulkarni-anurag <kanurag0801@gmail.com>
2021-07-05 18:06:06 +0530

    Column shrinking to minimum

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Container(
                 height: 100.0,
