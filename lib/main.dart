3a847795f394937cecb57bbf87cb7ff4b5cb403c
kulkarni-anurag <kanurag0801@gmail.com>
2021-07-05 18:12:25 +0530

    Containers positioned according to center crossalignment center

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Container(
                 height: 100.0,
