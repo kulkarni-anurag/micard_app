a1577ed14da99f54c8a8338dc6bc1336ca7e9217
kulkarni-anurag <kanurag0801@gmail.com>
2021-07-05 18:05:23 +0530

    Basic Column Layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,33 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.fromLTRB(10.0, 20.0, 40.0, 10.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello!'),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Center(
+                  child: Text('Container 1'),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.blue,
+                child: Center(
+                  child: Text('Container 2'),
+                ),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+                child: Center(
+                  child: Text('Container 3'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
