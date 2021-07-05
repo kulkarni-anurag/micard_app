edbcafde5433acc4984c54abc5d60e90c8c5c31b
kulkarni-anurag <kanurag0801@gmail.com>
2021-07-05 18:09:55 +0530

    Containers position evenly

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Container(
                 height: 100.0,
